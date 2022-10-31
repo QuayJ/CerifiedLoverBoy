7f5c3b8022a32b89cd40b72db5ce1f2d7c49ab3c
QuayJ <sjw.wilson@icloud.com>
2022-10-31 11:24:49 -0400

    replaced with a copy of correct folder.

--- Quay on PHP/index.php
+++ Quay on PHP/index.php
@@ -2,11 +2,12 @@
 <html>
 
     <head>
-        <link href="styles.css" rel="stylesheet">
+        <link href="css/styles.css" rel="stylesheet">
 
         <title>Become a Scorpion</title>
     </head>
     <body>
+        <!--THIS PAGE HAS THE SIGN UP-->
         
         <?php
         //This is a single line comment
@@ -15,12 +16,12 @@
             echo("ShaQuaya Wilson");
         ?>
   
-  <img src="Drake-Certified-Lover-Boy-1.jpeg" alt="Certified Lover Boy Logo" width="175">
+  <img src="images/Drake-Certified-Lover-Boy-1.jpeg" alt="Certified Lover Boy Logo" width="175">
 
 <h1>You're OVO bound</h1>
 <!-- Below; A Drake visual should be here -->
 <video width="450" height="240" autoplay muted>
-  <source src="Y2Mate.is - DJ Khaled ft. Drake & Lil Baby - STAYING ALIVE (Official Video)-PM2f835zx88-480p-1655501824650.mp4" type="video/mp4">
+  <source src="images/Y2Mate.is - DJ Khaled ft. Drake & Lil Baby - STAYING ALIVE (Official Video)-PM2f835zx88-480p-1655501824650.mp4" type="video/mp4" alt="Drake ft Lil Baby - Staying Alive Music Video">
 </video>
 <!-- This starts the subscribe process -->
 <form action="https://giphy.com/gifs/storyful-nba-drake-speech-7WvAUvZZTRpSuudobh/fullscreen" method="get">
@@ -37,7 +38,7 @@
             <!--When Is Your Birthday?
             <label for="birthday"><input type="date" id="birthday"></label>-->
 
-        <!--this is practice php would like to add age verification later on-->
+        <!--this is practice php would like to add age verification later on
             <?php
                 /*define ("LEGAL_AGE", 16);
                 $age=17;
@@ -74,6 +75,7 @@
                 
                 }
             ?>
+        
             <form action="index.php" method="POST">
                 Day:<select name="day">
                     <option>01</option>
@@ -111,7 +113,7 @@
                 Month:<select name="month">
                     <option value="01">Jan</option>
                     <option value="02">Feb</option>
-                    <option value="03">Mar/option>
+                    <option value="03">Mar</option>
                     <option value="04">Apr</option>
                     <option value="05">May</option>
                     <option value="06">Jun</option>
@@ -158,40 +160,23 @@
                 <option>2021</option> 
                 <option>2022</option> 
                 </select> 
-
+            -->
                   
             
                 <br><label for="paswword">Password:<input type="password" id="password" placeholder="6SixGod"></label>
 
                 <p><input type="submit" value="ENTER" name="submit"></p>
             </form>
-<!-- for table, maybe put circle pictures for the winner/loser or like ID pictures of opp/ally-->
-<table border="2">
-            <tr>
-                <th>The Opp</th>
-                <th>The Diss</th>
-                <th>Drake</th>
-                <th>W or L</th>
-            </tr>
-            <tr>
-                <td>Meek Millz</td>
-                <td>Wanna Know</td>
-                <td>Charged Up, Back to Back</td>
-                <td>Big L</td>
-            </tr>
-            <tr>
-                <td>Pusha T</td>
-                <td>Infared</td>
-                <td>Duppy</td>
-                <td>Hold that L</td>
-            </tr>
-        </table>
+
 
 
 
 
     <a href="index.html"><h2>Go to Papi's Home</h2></a>
-    <a href="secondpage.html"><h2>Return to Poll</h2></a>
+     <a href="pollindex.html"><h2>Return to Poll</h2></a>
+        <a href="history.html"><h2>Battle Rap</h2></a>
+        <a href="Team.html"><h2>OVO Team</h2></a>
+        <a href="Artists.html"><h2>OVO Sound</h2></a>
 
 
 <footer>
